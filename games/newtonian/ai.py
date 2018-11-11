620c81c424fd050b02fa58acba780550ec3c3ab6
csdhv9 <csdhv9@mst.edu>
2018-11-10 21:55:47 -0600

    Add parse map

--- games/newtonian/ai.py
+++ games/newtonian/ai.py
@@ -8,10 +8,10 @@ from joueur.base_ai import BaseAI
 # you can add additional import(s) here
 
 # Un-comment this line if you would like to use the debug map, which requires the colorama package.
-#if debug:
-#    from colorama import init, Fore, Back, Style
+if debug:
+    from colorama import init, Fore, Back, Style
 
-#from base_movements import *
+from base_movements import *
 
 # <<-- /Creer-Merge: imports -->>
 
@@ -51,60 +51,9 @@ class AI(BaseAI):
         """
         # <<-- Creer-Merge: start -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
         # replace with your start logic
-        class group:
-            def __init__(self, i_unit=None, m_unit=None, p_unit=None):
-                self.i_unit = i_unit
-                self.m_unit = m_unit
-                self.p_unit = p_unit
 
-
-                if len(self.units) == 3:
-                    self.phase = 1
-                    self.task = 'gather'
-                elif len(self.units) == 2:
-                    self.phase = 2
-                    self.task = 'hunt'
-                else:
-                    self.phase = 3
-                    self.task = 'link2'
-
-            @property
-            def units(self):
-                l = []
-                if self.i_unit: l.append(self.i_unit)
-                if self.m_unit: l.append(self.m_unit)
-                if self.p_unit: l.append(self.p_unit)
-
-                return l
-
-            def remove(self, unit):
-                if unit == self.i_unit: self.i_unit = None
-                if unit == self.m_unit: self.m_unit = None
-                if unit == self.p_unit: self.p_unit = None
-
-            def add(self, member):
-                type = member.job.title
-
-                if type is 'physicist':
-                    if self.p_unit is None:
-                        self.p_unit = member
-                        return True
-                elif type is 'manager':
-                    if self.m_unit is None:
-                        self.m_unit = member
-                        return True
-                elif type is 'intern':
-                    if self.i_unit is None:
-                        self.i_unit = member
-                        return True
-                else:
-                    print("Member has type i don't know how to read -> class group")
-
-                return False
-
-        self.groups = []
         # Un-comment this line if you are using colorama for the debug map.
-        # init
+        # init()
 
         # <<-- /Creer-Merge: start -->>
 
@@ -115,61 +64,6 @@ class AI(BaseAI):
         # <<-- Creer-Merge: game-updated -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
         # replace with your game updated logic
         # <<-- /Creer-Merge: game-updated -->>
-
-    def group_update(self):
-        for group in self.groups:
-            for member in group.units:
-                # See if member is dead
-                if member == None or not member.health:
-                    group.remove(member)
-
-            if not len(group.units):
-                self.groups.remove(group)
-                continue
-
-            group.phase = (-(len(group.units) - 2) + 2)
-
-        agents_in_group = [agent for agent in group for group in self.groups]
-
-        agents_to_add = []
-        for agent in self.player.units:
-            if agent not in agents_in_group:
-                agents_to_add.append(agent)
-
-        for agent in agents_to_add:
-            for group in self.groups:
-                if group.add(member):
-                    agents_to_add.remove(agent)
-                    break
-
-        # add agent to group somehow
-
-        # if a group in phase 2 or 3 is found append it to a temporary list
-        # look through the list of phase 2 and 3 groups and see if there are any
-        # combinations to make a phase 1 or 2 IF so initialize a new group with
-        # all of new members and delete the old groups
-
-    def group_logic(self, group):
-        if group.phase == 1:
-            phase_1(group)
-        elif group.phase == 2:
-            phase_2(group)
-        else:
-            phase_3(group)
-
-    def phase_1(self, group):
-        if group.task == 'gather':
-            #intern leads the group while gathering 4 resources
-            #When the intern has 4 resources the function returns True
-            # and the task is witch to 'refine'
-        elif group.task == 'refine':
-            #intern leads the group to the refinery, then the physicist refines
-            #materials until they are all refined, switches to 'generate' after This
-            #occurs
-        else group.task == 'generate':
-            #Manager and physicist fill their inventorys with refined materials
-            #and manager leads to generator. After this task is completed switch
-            #back to 'gather task'
 
     def end(self, won, reason):
         """ This is called when the game ends, you can clean up your data and
@@ -197,8 +91,7 @@ class AI(BaseAI):
         Please note: This code is intentionally bad. You should try to optimize everything here. THe code here is only to show you how to use the game's
                      mechanics with the MegaMinerAI server framework.
         """
-        self.group_update()
-        
+
         # Goes through all the units that you own.
         for unit in self.player.units:
             # Only tries to do something if the unit actually exists.
@@ -488,6 +381,45 @@ class AI(BaseAI):
 
         return
 
+    def parsefield(map):
+        output = {
+        'blueium': [], 'redium': [], 'enemy': [], 'team': [], 'refinery': [],
+        'generator': [], 'spawn': []
+        }
+
+        for tile in map:
+            if tile.blueium > 0:
+                output['blueium'].append(tile)
+                pass
+
+            if tile.redium > 0:
+                output['redium'].append(title)
+                pass
+
+            if tile.machine:
+                output['refinery'].append(tile)
+                pass
+
+            if tile.type == 'generator':
+                output['generator'].append(tile)
+                pass
+
+            if tile.type == 'conveyor':
+                output['conveyor'].append(tile)
+                pass
+
+            if tile.type == 'spawn':
+                output['spawn'].append(tile)
+                pass
+
+            if tile.unit:
+                if tile.unit.owner == self.player.enemy:  # TODO - double check
+                    output['team'].append(tile)
+                else:
+                    output['enemy'].append(tile)
+
+
+
     def action(self, keywd, tile, unit, path):
         path = self.find_path(unit.tile, tile)
         if keywd != 'move':
@@ -502,7 +434,7 @@ class AI(BaseAI):
         elif keywd != 'move':
             unit.act(tile)
 
-        if unit.acted == True or keywd == 'move':
+        if unit.acted == True || keywd == 'move':
             return True
         else:
             return False
